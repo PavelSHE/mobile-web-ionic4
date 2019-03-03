a2d942c5d9ab2be9e3ec5b9041268165b5691875
pavel.sheinfeld@gmail.com <1good morning>
2019-03-03 16:06:34 +0200

    home page platform button test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController, AlertController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController, private platform: Platform) {
+ 
+  }
+ 
+ 
+
+  async showPlatform() {
+    let text = 'I run on: ' + this.platform.platforms();
+    const alert = await this.alertCtrl.create({
+      header: 'My Home',
+      subHeader: text,
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
 
 }
