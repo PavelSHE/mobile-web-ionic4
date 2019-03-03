72aaac7888b6452cf7d3143d572eda8c74cb2e13
pavel.sheinfeld@gmail.com <1good morning>
2019-03-03 17:12:32 +0200

    login,register pages added

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCvPZqSFJu6yoh1olwANmoIZj_0NwqTZfw",
+    authDomain: "cottonsugar-e1aff.firebaseapp.com",
+    databaseURL: "https://cottonsugar-e1aff.firebaseio.com",
+    projectId: "cottonsugar-e1aff",
+    storageBucket: "cottonsugar-e1aff.appspot.com",
+    messagingSenderId: "903408055843"
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from "angularfire2";
+import { FIREBASE_CONFIG } from './app.firebase.config';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(FIREBASE_CONFIG)],
   providers: [
     StatusBar,
     SplashScreen,
@@ -20,4 +23,4 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
