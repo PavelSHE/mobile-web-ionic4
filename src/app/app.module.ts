de12c5336f0c60bfe7f344b3d7132834a4d1ed4b
pavel.sheinfeld@gmail.com <1good morning>
2019-03-06 17:54:48 +0200

    Box page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AngularFireModule } from "@angular/fire";
 import { FIREBASE_CONFIG } from './app.firebase.config';
 
-import { AngularFireAuthModule} from "angularfire2/auth";
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(FIREBASE_CONFIG)],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(FIREBASE_CONFIG),AngularFireDatabaseModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController, AlertController, Platform } from '@ionic/angular';
+import { CottonSugarBox } from 'src/models/cottonsugarbox'
+import { AngularFireList, AngularFireDatabase } from "angularfire2/database";
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from "angularfire2/auth";
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +12,43 @@ import { NavController, AlertController, Platform } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(public navCtrl: NavController, private alertCtrl: AlertController, private platform: Platform) {
- 
+  //box = { } as CottonSugarBox;
+  data: Observable<any[]>;
+  ref: AngularFireList<any>;
+  dataPath: string;
+  boxes : [];
+
+  constructor(private afAuth: AngularFireAuth, private db: AngularFireDatabase, private alertCtrl: AlertController, private platform: Platform, public navCtrl: NavController) {
+    //this.box.id = "bla1";
+    this.boxes = [];
   }
- 
- 
+
+  ngOnInit(){
+    this.afAuth.authState.subscribe(data =>{
+      if (data && data.email && data.uid) {
+        this.dataPath = "/boxes/" + btoa(data.email).replace(/=/g, "-") + "/";
+        this.ref = this.db.list(this.dataPath);//,ref => ref.orderByKey());
+        this.ref.valueChanges().subscribe(result => {
+          if (this.boxes.length > 0){
+            console.log("updates")
+          }else{
+            console.log("creates");
+            this.showBoxes(result);
+          }
+        })
+      }else{
+        //to do data guards
+      }
+    });
+  } 
+  
+  showBoxes(data){
+    this.boxes = data;
+    // Object.keys(data).map(function(key, index) {
+    //   data[key] *= 2;
+    //});
+    console.log(this.boxes);
+  }
 
   async showPlatform() {
     let text = 'I run on: ' + this.platform.platforms();
@@ -24,4 +61,9 @@ export class HomePage {
     await alert.present();
   }
 
+  navigateToBoxLogs(_box){
+    console.log(_box);
+  }
+
 }
+
